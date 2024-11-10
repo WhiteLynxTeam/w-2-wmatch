6baa8d6fb67406e8c5f10343b526fffa747d172d
Yuris <yuris.musin@mail.ru>
2024-11-10 15:56:41 +0500

    добавил модальные окна по клику на вопросики

--- app/src/main/java/ru/w_2_wmatch/view/questionnaire/key_brand_values/KeyBrandValuesFragment.kt
+++ app/src/main/java/ru/w_2_wmatch/view/questionnaire/key_brand_values/KeyBrandValuesFragment.kt
@@ -9,8 +9,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.PopupWindow
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.w_2_wmatch.R
 import ru.w_2_wmatch.databinding.FragmentKeyBrandValuesBinding
 
@@ -42,11 +46,11 @@ class KeyBrandValuesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.ivHelpKeyValues.setOnClickListener {
-            showPopupWindow(it)
+            showPopupWindow(it, "В зависимости от выбранных ценностей алгоритм подберет релевантные бренды.")
         }
 
         binding.ivHelpUniqBrand.setOnClickListener {
-            showPopupWindow(it)
+            showPopupWindow(it, "Например: «Яркая детская одежда с принтами ручной работы»")
         }
 
         with(binding) {
@@ -94,20 +98,23 @@ class KeyBrandValuesFragment : Fragment() {
     }
 
     @SuppressLint("InflateParams", "MissingInflatedId")
-    private fun showPopupWindow(anchorView: View) {
+    private fun showPopupWindow(anchorView: View, text: String) {
         // Создаем инфлятор для наших popup-элементов
         val inflater = layoutInflater
-        val popupView = inflater.inflate(R.layout.help_info_builder, null)
+        val popupView = inflater.inflate(R.layout.help_info_key_values, null)
+
+        val string: TextView = popupView.findViewById(R.id.text)
+        string.text = text
 
         // Создаем PopupWindow
         val popupWindow = PopupWindow(popupView,
             LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT)
 
-        // Устанавливаем кнопку закрытия
-        val closeButton: Button = popupView.findViewById(R.id.btn_send)
-        closeButton.setOnClickListener {
-            popupWindow.dismiss() // Закрываем всплывающее окно
+        //закрываем окно через 2 секунды
+        lifecycleScope.launch {
+            delay(2000)
+            popupWindow.dismiss()
         }
 
         // Устанавливаем PopupWindow рядом с элементом
