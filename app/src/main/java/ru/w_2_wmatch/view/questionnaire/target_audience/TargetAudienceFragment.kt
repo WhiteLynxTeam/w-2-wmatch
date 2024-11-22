41fd9df301178f9751700a3e26e5224f6996ac4b
Yuris <yuris.musin@mail.ru>
2024-11-22 14:00:54 +0500

    реализовал показ значений Seekbar в фрагменте целевая аудитория

--- app/src/main/java/ru/w_2_wmatch/view/questionnaire/target_audience/TargetAudienceFragment.kt
+++ app/src/main/java/ru/w_2_wmatch/view/questionnaire/target_audience/TargetAudienceFragment.kt
@@ -1,9 +1,12 @@
 package ru.w_2_wmatch.view.questionnaire.target_audience
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import me.tankery.lib.circularseekbar.CircularSeekBar
 import ru.w_2_wmatch.R
 import ru.w_2_wmatch.databinding.FragmentTargetAudienceBinding
 import ru.w_2_wmatch.view.base.BaseFragment
@@ -28,6 +31,45 @@ class TargetAudienceFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.progressAgeWoman.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                // Показываем текстовое значение во время прокрутки
+                binding.progressAgeWomanValue.text = progress.toString()
+                binding.progressAgeWomanValue.visibility = View.VISIBLE
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // действия при начале прокрутки (если нужно)
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Скрываем текстовое значение после завершения прокрутки
+                binding.progressAgeWomanValue.visibility = View.GONE
+            }
+        })
+
+
+        binding.progressGenderMan.setOnSeekBarChangeListener(object :
+            CircularSeekBar.OnCircularSeekBarChangeListener {
+
+            override fun onProgressChanged(
+                circularSeekBar: CircularSeekBar?,
+                progress: Float,
+                fromUser: Boolean
+            ) {
+                val value = progress.toInt().toString()
+                binding.percentGenderMan.text = "$value %"
+            }
+
+            override fun onStartTrackingTouch(seekBar: CircularSeekBar?) {
+                // Можно добавить дополнительную логику, если нужно
+            }
+
+            override fun onStopTrackingTouch(seekBar: CircularSeekBar?) {
+                // Можно добавить дополнительную логику, если нужно
+            }
+        })
 
     }
 }
