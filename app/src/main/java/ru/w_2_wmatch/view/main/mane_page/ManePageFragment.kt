6f9262a2775e5765c916cf7f62a7fd1216f06fe3
Edgar <Meccu9i@gmail.com>
2024-12-01 21:25:38 +0300

    добавил spinner для выбора статистики за определенный период

--- app/src/main/java/ru/w_2_wmatch/view/main/mane_page/ManePageFragment.kt
+++ app/src/main/java/ru/w_2_wmatch/view/main/mane_page/ManePageFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import ru.w_2_wmatch.R
 import ru.w_2_wmatch.databinding.FragmentManePageBinding
 import ru.w_2_wmatch.domain.models.Community
@@ -25,29 +28,88 @@ class ManePageFragment : BaseFragment() {
     private lateinit var viewModel: ManePageViewModel
 
     private val community = listOf(
-        Community(img = R.drawable.img_communiti, title = "Как создать первую коллабу?" , description = "Мы хотим подарить женщинам ВРЕМЯ, которое они ежедневно тратят на выбор подходящей одежды" ),
-        Community(img = R.drawable.img_communiti, title = "Как создать первую коллабу?" , description = "Мы хотим подарить женщинам ВРЕМЯ, которое они ежедневно тратят на выбор подходящей одежды" ),
-        Community(img = R.drawable.img_communiti, title = "Как создать первую коллабу?" , description = "Мы хотим подарить женщинам ВРЕМЯ, которое они ежедневно тратят на выбор подходящей одежды" ),
-        Community(img = R.drawable.img_communiti, title = "Как создать первую коллабу?" , description = "Мы хотим подарить женщинам ВРЕМЯ, которое они ежедневно тратят на выбор подходящей одежды" ),
+        Community(
+            img = R.drawable.img_communiti,
+            title = "Как создать первую коллабу?",
+            description = "Мы хотим подарить женщинам ВРЕМЯ, которое они ежедневно тратят на выбор подходящей одежды"
+        ),
+        Community(
+            img = R.drawable.img_communiti,
+            title = "Как создать первую коллабу?",
+            description = "Мы хотим подарить женщинам ВРЕМЯ, которое они ежедневно тратят на выбор подходящей одежды"
+        ),
+        Community(
+            img = R.drawable.img_communiti,
+            title = "Как создать первую коллабу?",
+            description = "Мы хотим подарить женщинам ВРЕМЯ, которое они ежедневно тратят на выбор подходящей одежды"
+        ),
+        Community(
+            img = R.drawable.img_communiti,
+            title = "Как создать первую коллабу?",
+            description = "Мы хотим подарить женщинам ВРЕМЯ, которое они ежедневно тратят на выбор подходящей одежды"
+        ),
     )
 
     private val media = listOf(
-        Media(img = R.drawable.img_media1, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
-        Media(img = R.drawable.img_media2, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
-        Media(img = R.drawable.img_media1, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
-        Media(img = R.drawable.img_media2, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
+        Media(
+            img = R.drawable.img_media1,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
+        Media(
+            img = R.drawable.img_media2,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
+        Media(
+            img = R.drawable.img_media1,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
+        Media(
+            img = R.drawable.img_media2,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
     )
     private val news = listOf(
-        NewsEducation(img = R.drawable.img_news1, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
-        NewsEducation(img = R.drawable.img_news1, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
-        NewsEducation(img = R.drawable.img_news1, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
+        NewsEducation(
+            img = R.drawable.img_news1,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
+        NewsEducation(
+            img = R.drawable.img_news1,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
+        NewsEducation(
+            img = R.drawable.img_news1,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
     )
     private val education = listOf(
-        NewsEducation(img = R.drawable.img_education1, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
-        NewsEducation(img = R.drawable.img_education1, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
-        NewsEducation(img = R.drawable.img_education1, title = "The Wellnus & Sarro" , description = "Познакомьтесь c будущими партнерам"),
+        NewsEducation(
+            img = R.drawable.img_education1,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
+        NewsEducation(
+            img = R.drawable.img_education1,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
+        NewsEducation(
+            img = R.drawable.img_education1,
+            title = "The Wellnus & Sarro",
+            description = "Познакомьтесь c будущими партнерам"
+        ),
     )
 
+    val periods = listOf(
+        "За выбранный период", "Период 1", "Период 2", "Период 3", "Период 4", "Период 5"
+    )
 
 
     override fun onCreateView(
@@ -81,5 +143,24 @@ class ManePageFragment : BaseFragment() {
         newsEducationAdapter = NewsEducationAdapter(education)
         binding.rcEducation.adapter = newsEducationAdapter
 
+        val spinnerAdapter = ArrayAdapter(
+            requireContext(),
+            R.layout.custom_spinner_item,// Указываем кастомный макет
+            periods
+        )
+
+        // Применяем адаптер к Spinner
+        binding.tvPeriod.adapter = spinnerAdapter
+
+        // Обработка выбора элемента
+        binding.tvPeriod.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Когда ничего не выбрано
+            }
+        }
     }
 }
